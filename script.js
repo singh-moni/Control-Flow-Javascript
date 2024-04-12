c856d643e6122505fe454bb16a039cfd829adc85
singh-moni <moni.rinki@gmail.com>
2024-04-12 17:48:12 -0400

    Part 1

---
+++ script.js
@@ -0,0 +1,41 @@
+let totalArea = 3.1415 * 5 *5
+console.log(totalArea)//78.53
+let TotalCapacity = totalArea/0.8
+console.log(TotalCapacity)//98.17
+
+let week = 1
+if( growth = 20 * 2 * week) 
+{
+console.log(`${growth} : Week 1 Growth`)}
+
+if ( growth > TotalCapacity * 0.8){
+    console.log("Pruned : Week 1")
+} else if (growth> TotalCapacity * 0.5){
+    console.log("Monitored : Week 1")
+} else { console.log("Planted : Week 1")}
+
+
+let week2 = 2
+if (growth2 = 20 * 2 * week2)
+{
+    console.log(`${growth2} : Week 2 Growth`)}
+
+if ( growth2 > TotalCapacity * 0.8){
+    console.log("Pruned : Week 2")
+} else if (growth2> TotalCapacity * 0.5){
+    console.log("Monitored : Week 2")
+} else { console.log("Planted : Week 2")}
+
+let week3 = 3
+if (growth3 = 20 * 2 * week3) {
+    console.log(`${growth3} : Week 3 Growth`)
+}
+if ( growth3 > TotalCapacity * 0.8){
+    console.log("Pruned : Week 3")
+} else if (growth3> TotalCapacity * 0.5){
+    console.log("Monitored : Week 3")
+} else { console.log("Planted : Week 3")}
+
+    
+        
+ 
